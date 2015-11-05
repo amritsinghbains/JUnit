96bba0d5c22efbd51e5eff172c999a732ea19cad
amrit singh bains <amrit.singh.bains@live.com>
2015-11-05 18:54:18 -0500

    schedule test for 8 hours

--- src/Junit/CMeeting.java
+++ src/Junit/CMeeting.java
@@ -12,4 +12,16 @@ public class CMeeting {
 		}
 		return result;		
 	}
+	public void setTimes(int h1, int m1, int h2, int m2){
+		this.start = new CTime();
+		this.end = new CTime();
+		this.start.hour = h1;
+		this.start.minute = m1;
+		this.end.hour = h2;
+		this.end.minute = m2;		
+	}
+//	public static void main(String args[]){
+//		CMeeting cm = new CMeeting();
+//		cm.setTimes(0 , 1 , 2 , 3);
+//	}
 }
--- src/Junit/CShcheduleTest.java
+++ src/Junit/CShcheduleTest.java
@@ -1,18 +1,47 @@
 package Junit;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class CShcheduleTest {
 	public void setScheduleTest()
 	{
-	CSchedule cs = new CSchedule();
-	CMeeting cm = new CMeeting();
-	CTime ct = new CTime();
-	
-	
-	//cm.checkValid(1, 2);
-	
+		CSchedule cs = new CSchedule();
+		CMeeting cm = new CMeeting();
+		CTime ct = new CTime();	
 	}
 	
-	
+	@Test
+	public void checkTotalMeetingsTest(){
+		CMeeting[] m = new CMeeting[10];
+		for(int i=0; i<10; i++){
+			m[i] = new CMeeting();
+		}
+		
+		m[0].setTimes(1, 0, 2, 0);
+		m[1].setTimes(2, 0, 3, 0);
+		m[2].setTimes(3, 30, 4, 0);
+		m[3].setTimes(4, 0, 5, 0);
+		m[4].setTimes(5, 30, 6, 0);
+		m[5].setTimes(6, 0, 7, 0);
+		m[6].setTimes(7, 0, 8, 30);
+		m[7].setTimes(8, 30, 9, 0);
+		m[8].setTimes(9, 0, 10, 0);
+		m[9].setTimes(10, 0, 11, 30);
+		
+		CSchedule cs = new CSchedule();
+		CMeeting cm[] = new CMeeting[]{m[0]};
+		assertTrue(cs.checkTotalMeetings(cm));
+		cm = new CMeeting[]{m[0], m[1], m[2]};
+		assertTrue(cs.checkTotalMeetings(cm));
+		cm = new CMeeting[]{m[3], m[4], m[5], m[6], m[7], m[8], m[9] };
+		assertTrue(cs.checkTotalMeetings(cm));
+		cm = new CMeeting[]{m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8], m[9]};
+		assertFalse(cs.checkTotalMeetings(cm));
+		
 	}
+	
+}
 
 
