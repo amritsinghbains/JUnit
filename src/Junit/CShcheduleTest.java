0479a78f80418b09ac8a61f04f736de7eae56a87
Danish <d.saleem91@gmail.com>
2015-11-05 17:41:26 -0500

    schedule incomplete

---
+++ src/Junit/CShcheduleTest.java
@@ -0,0 +1,18 @@
+package Junit;
+
+public class CShcheduleTest {
+	public void setScheduleTest()
+	{
+	CSchedule cs = new CSchedule();
+	CMeeting cm = new CMeeting();
+	CTime ct = new CTime();
+	
+	
+	//cm.checkValid(1, 2);
+	
+	}
+	
+	
+	}
+
+
--- src/Junit/CTimeTest.java
+++ src/Junit/CTimeTest.java
@@ -13,7 +13,7 @@ public class CTimeTest {
 			{
 				for (int m = -100; m<100; m++)
 				{
-					System.out.println("Hours: " + h + "Minutes: " + m );
+					System.out.println("Hours: " + h + " Minutes: " + m );
 					
 					if(h<0 && m<0)
 					{
